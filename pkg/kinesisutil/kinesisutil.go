73ba69ebf1c3908f66d34a554e5f2a2744563653
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-07-03 00:25:02 +0600

    Add kinesis CRUD to work with streams and messages

--- pkg/kinesisutil/kinesisutil.go
+++ pkg/kinesisutil/kinesisutil.go
@@ -21,11 +21,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/knative/eventing/pkg/provisioners"
 	"go.uber.org/zap"
 )
 
 // Connect creates a new Kinesis-Streaming connection
-func Connect(accountAccessKeyID, accountSecretAccessKey, region, streamName string, logger *zap.SugaredLogger) (*kinesis.Kinesis, error) {
+func Connect(accountAccessKeyID, accountSecretAccessKey, region string, logger *zap.SugaredLogger) (*kinesis.Kinesis, error) {
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(region),
 		Credentials: credentials.NewStaticCredentials(accountAccessKeyID, accountSecretAccessKey, ""),
@@ -37,16 +38,63 @@ func Connect(accountAccessKeyID, accountSecretAccessKey, region, streamName stri
 	}
 
 	kinesis := kinesis.New(sess)
-	logger.Infof("Connect(): connection to Kinesis stream established, Conn=%+v", kinesis)
+	logger.Infof("Connect(): connection to Kinesis established, Conn=%+v", kinesis)
 	return kinesis, nil
 }
 
-// Publish publishes msg to Kinesis
-func Publish(client *kinesis.Kinesis, partitionKey, streamName *string, msg []byte, logger *zap.SugaredLogger) error {
+func Exist(client *kinesis.Kinesis, streamName *string) (bool, error) {
+	_, err := client.DescribeStream(&kinesis.DescribeStreamInput{
+		StreamName: streamName,
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+	//TODO maybe need to check stream status, if it is not ACTIVE, return false and no error, or
+	// should probably return stream in the responce to check status and other params later in logic
+}
+
+func Create(client *kinesis.Kinesis, streamName *string) error {
+	_, err := client.CreateStream(&kinesis.CreateStreamInput{
+		ShardCount: aws.Int64(1), // by now creating streams with only one shard.
+		StreamName: streamName,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Delete(client *kinesis.Kinesis, streamName *string) error {
+	_, err := client.DeleteStream(&kinesis.DeleteStreamInput{
+		EnforceConsumerDeletion: aws.Bool(true), // by now creating streams with only one shard.
+		StreamName:              streamName,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Publish publishes msg to Kinesis stream
+func Publish(client *kinesis.Kinesis, streamName *string, msg []byte, logger *zap.SugaredLogger) error {
 	_, err := client.PutRecord(&kinesis.PutRecordInput{
 		Data:         msg,
-		PartitionKey: partitionKey,
+		PartitionKey: streamName,
 		StreamName:   streamName,
 	})
 	return err
+}
+
+func GetRecord(client *kinesis.Kinesis, shardIterator *string) (provisioners.Message, *string, error) {
+	var message provisioners.Message
+	res, err := client.GetRecords(&kinesis.GetRecordsInput{
+		Limit:         aws.Int64(1),
+		ShardIterator: shardIterator,
+	})
+	if err != nil {
+		return message, nil, err
+	}
+	message.Payload = res.Records[0].Data
+	return message, res.NextShardIterator, nil
 }
